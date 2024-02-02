3b9d0e5e4cb81d42268578c0afab4ac4f448b64d
Skye McLean <skyemclean87@gmail.com>
2024-02-02 13:08:26 -0800

    Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='title'>
+        Destination <strong>You</strong>
+      </div>
+      <div className='nav-menu'>
+        <div className='navline-1'></div>
+        <div className='navline-2'></div>
+        <div className='navline-3'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
