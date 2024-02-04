8fec38ff7a4f8f994f05f86986f69fe3f3833733
Skye McLean <skyemclean87@gmail.com>
2024-02-03 16:38:59 -0800

    Header and header image added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { Link } from 'react-router-dom'
 import Menu from './components/Menu';
+import Home from './components/Home';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -9,7 +10,7 @@ function App() {
   return (<Router>
     <div className="App">
       <Navbar />
-      
+      <Home />
     </div>
     </Router>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='header'>
+        <h2 className='header-text'>Escape</h2>
+        <p className='header-text'>Dreamy and enticing getaways for singles. Find the best deals on your ideal solo adventure.</p>
+        <div className='learn-btn'>Browse Deals</div>
+      </div>
+      <div className='header-img-wrapper'>
+      <img className="header-img" src="https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_620/MTc2MjY4NDcxMDE2OTU3MTAx/tropical-beach-pictures.webp" alt="beach"></img>
+      </div>
+    </div>
+  )
+}
+
+export default Home
